0b5a6f8223fd1b14a584f4cc012542fa08898bf0
mohamedsayari <58943388+mohamedsayari@users.noreply.github.com>
2021-02-21 21:16:25 +0100

    Add files via upload

---
+++ backEm/src/main/java/com/example/demo/Model/Employee.java
@@ -0,0 +1,46 @@
+package com.example.demo.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
+	@NotNull
+    private String firstName;
+	@NotNull
+    private String lastName;
+	@NotNull
+    private String emailId;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+
+}
